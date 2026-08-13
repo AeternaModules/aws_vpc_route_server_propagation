b964c4ee265c670a4e715e0e130ef20676b91764
Daniel Allen <dan@aeternagroup.net>
2026-08-13 16:52:26 +0100

    Update to schema version v6.58.0

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "vpc_route_server_propagations_id" {
+  description = "Map of id values across all vpc_route_server_propagations, keyed the same as var.vpc_route_server_propagations"
+  value       = { for k, v in aws_vpc_route_server_propagation.vpc_route_server_propagations : k => v.id if v.id != null && length(v.id) > 0 }
+}
+output "vpc_route_server_propagations_region" {
+  description = "Map of region values across all vpc_route_server_propagations, keyed the same as var.vpc_route_server_propagations"
+  value       = { for k, v in aws_vpc_route_server_propagation.vpc_route_server_propagations : k => v.region if v.region != null && length(v.region) > 0 }
+}
+output "vpc_route_server_propagations_route_server_id" {
+  description = "Map of route_server_id values across all vpc_route_server_propagations, keyed the same as var.vpc_route_server_propagations"
+  value       = { for k, v in aws_vpc_route_server_propagation.vpc_route_server_propagations : k => v.route_server_id if v.route_server_id != null && length(v.route_server_id) > 0 }
+}
+output "vpc_route_server_propagations_route_table_id" {
+  description = "Map of route_table_id values across all vpc_route_server_propagations, keyed the same as var.vpc_route_server_propagations"
+  value       = { for k, v in aws_vpc_route_server_propagation.vpc_route_server_propagations : k => v.route_table_id if v.route_table_id != null && length(v.route_table_id) > 0 }
+}
+
